31db5f6d9f68c2ee34eb707a8cf13091d4ae18a1
husee-code <87664267+husee-code@users.noreply.github.com>
2023-10-19 10:53:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ $.get('https://openexchangerates.org/api/latest.json', {
     document.getElementById("rub9").innerHTML = (eur_to_rub*rub9).toFixed(1);
     document.getElementById("rub10").innerHTML = (eur_to_rub*rub10).toFixed(1);
     document.getElementById("rub11").innerHTML = (eur_to_rub*rub11).toFixed(1);
+    document.getElementById("rub12").innerHTML = (eur_to_rub*rub12).toFixed(1);
 
     document.getElementById('usdt1').innerHTML = (eur_to_usdt*usdt1).toFixed(3);
     document.getElementById('usdt2').innerHTML = (eur_to_usdt*usdt2).toFixed(3);
@@ -32,5 +33,5 @@ $.get('https://openexchangerates.org/api/latest.json', {
     document.getElementById('usdt9').innerHTML = (eur_to_usdt*usdt9).toFixed(3);
     document.getElementById('usdt10').innerHTML = (eur_to_usdt*usdt10).toFixed(3);
     document.getElementById('usdt11').innerHTML = (eur_to_usdt*usdt11).toFixed(3);
-    document.getElementById('usdt12').innerHTML = (eur_to_usdt*usdt12).toFixed(3);
+    document.getElementById('usdt11').innerHTML = (eur_to_usdt*usdt12).toFixed(3);
 });
